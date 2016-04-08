84a95d0a32a3d45d15a4f0f952ecd8c7a7e5097f
辉 <@172.16.40.87>
2016-04-08 15:57:11 +0800

    增加基础实体类

--- src/main/java/com/j1/health/model/Category.java
+++ src/main/java/com/j1/health/model/Category.java
@@ -1,5 +1,10 @@
 package com.j1.health.model;
 
-public class Category {
+import org.apache.ibatis.type.Alias;
+
+import com.j1.health.model.base.CommonEntity;
+
+@Alias(value = "category")
+public class Category extends CommonEntity{
 
 }
