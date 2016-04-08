e7724511101a801d93f4d4dacefdb4fb28597463
辉 <@172.16.40.87>
2016-04-08 16:40:23 +0800

    增加配置文件

--- src/main/java/com/j1/health/dao/CategoryDao.java
+++ src/main/java/com/j1/health/dao/CategoryDao.java
@@ -1,7 +1,9 @@
 package com.j1.health.dao;
 
 import com.j1.health.dao.base.CrudDao;
+import com.j1.health.dao.base.MyBatisRepository;
 
+@MyBatisRepository
 public interface CategoryDao extends CrudDao{
 
 }
